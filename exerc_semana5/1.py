f6c46605b7f3da601ed22cebaa919b316c793464
Filipi Inácio Penha dos Santos <filipiinacio25251120@gmail.com>
2023-09-19 18:32:18 -0300

    committed

---
+++ exerc_semana5/1.py
@@ -0,0 +1,9 @@
+import datetime
+
+ano_atual = datetime.datetime.now().year
+
+#salário inicial e o ano de contratação
+salario_inicial = float(input("Digite o salário inicial do funcionário: R$"))
+ano_contratacao = int(input("Digite o ano de contratação do funcionário: "))
+
+#ver se salário inicial atende ao critério mínimo
