4d26b98166737b0563253a9b6c272ad0975238c8
Filipi Inácio Penha dos Santos <filipiinacio25251120@gmail.com>
2023-09-19 19:30:15 -0300

    2.week5

--- exerc_semana5/2.py
+++ exerc_semana5/2.py
@@ -1,9 +1,25 @@
 import random
 
-def difficult_easy():
-    n = random.randint(1, 10)
-    adivinhe = int(input('Adivinhe o número de 1 a 10?'))
-    while (n != adivinhe):
-       if (n > adivinhe):
-          print(f'Errado, o número {adivinhe} é maior que o correto. Tente novamente')
-          
+def jogo(nivel):
+    if nivel == 'FÁCIL' :
+       limite = 10
+    elif nivel == 'MÉDIO':
+       limite = 50
+    elif nivel == 'DIFÍCIL':
+       limite = 100
+    else:
+       print('Nivel inválido. ')
+       return
+    
+    n = random.randint(1, limite)
+    while True:
+        valor = int(input('Digite um valor até o número correto: '))
+        x += 1
+        if valor == n:
+            print('Parabéns voce acertou!')
+            print(f'Foram {x} tentativas até o acerto!')
+            break
+        elif valor < n:
+            print('Um pouco mais!')
+        else:
+         print('Um pouco menos!')
