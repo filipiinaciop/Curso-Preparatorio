b60f7896a7a97ac045364fefe23ba589f3cc282a
Filipi Inácio Penha dos Santos <filipiinacio25251120@gmail.com>
2023-09-21 16:23:43 -0300

    exercs.week5

--- exerc_semana5/1.py
+++ exerc_semana5/1.py
@@ -3,18 +3,18 @@ import datetime
 salario_inicial = float(input('Digite o salário inicial do funcionário: $'))
 ano = int(input('Digite o ano de contratação do funcionário: '))
 
-al = 0.015 
+pa = 0.015 
 
 hoje = datetime.datetime.now().year #date 2023
 
 #NO PRIMEIRO ANO O PERCENTUAL DE AUMENTO SERA DE 1,5%, A PARTIR DO SEGUNDO ANO OS AUMENTOS SALARIAIS SERAO PROGRESSIVOS COM UM ACRESIMO DE 10% SOBRE O PERCENTUAL DO AUMENTO ANTERIOR.DEVE-SE EXIBIR  O SALARIO ATUAL DO FUNCIONARIO E QUANTOS % TEVE EM RELACAO AO SALARIO INICIAL
 
 while ano < hoje:
-    aumento = salario_inicial * al
+    aumento = salario_inicial * pa
     salario_inicial += aumento
-    al += 0.015
+    pa += 0.015
     ano += 1
 
-fim1 = ((salario_inicial - 1000) / 1000) * 100
+total_aumento = ((salario_inicial - 1000) / 1000) * 100
 print(f'O salário atual do funcionário é ${salario_inicial:.2f}')  #Salario final arrendondado
-print(f'A porcentagem total em relação ao salário inicial é {fim1:.2f}%')
+print(f'A porcentagem total em relação ao salário inicial é {total_aumento:.2f}%')
