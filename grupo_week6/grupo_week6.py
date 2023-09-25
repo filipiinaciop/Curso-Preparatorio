b5dacdee160f198b1fdd968e8c00de8972753bb6
Filipi Inácio Penha dos Santos <filipiinacio25251120@gmail.com>
2023-09-25 18:45:25 -0300

    é.grupo_week6

---
+++ grupo_week6/grupo_week6.py
@@ -0,0 +1,17 @@
+#grupo 1 sala t2
+#FIlipi Inácio Penha dos santos, # João Pedro carvalho de Jesus, Laura Eduarda Blauth, Giulia Harquihara kroiss
+
+mensagem = input('Mensagem que deseja criptografar: ')
+chave = int(input('Chave entre (1, 9): '))
+alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+def descriptografar():
+    codificado = []
+    str = ""
+    mensagem = input("Escreva a mensagem para descriptografar: ")
+
+def criptografar():
+    codificado = []
+    str = ""
+    mensagem = input("Escreva a mensagem para criptografar: ")
+    mensagem = mensagem.lower()
+    mensagem = mensagem.lower()
