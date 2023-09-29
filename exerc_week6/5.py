def71f33cea7a4ad7b12783f35a58950001f7e20
Filipi Inácio Penha dos Santos <filipiinacio25251120@gmail.com>
2023-09-29 20:17:26 -0300

    exerc_week6

--- exerc_week6/5.py
+++ exerc_week6/5.py
@@ -8,15 +8,15 @@ percas = 0
 valor = 100.00
 while True:
     aposta = float(input('Digite o valor de sua aposta: '))
-    d1 = random.randint(1, 6)
-    d2 = random.randint(1, 6)
-    d3 = d1 + d2
+    n1 = random.randint(1, 6)
+    n2 = random.randint(1, 6)
+    n3 = n1 + n2
     if aposta > valor:
         print(f'O valor apostado não pode ser superior ao saldo atual \033[33m{valor:.2f}\033[0m: Jogadas ganhas {ganhas} perdidas {percas}')
         break
-    elif d3 == 7 and 11:
+    elif n3 == 7 and 11:
         ganhas += 1
-        print(f'São {d1} e {d2} os valores dos dados jogados')
+        print(f'São {n1} e {n2} os valores dos dados jogados')
         aposta = aposta * 2
         saldo = aposta + valor
         valor = saldo
@@ -29,8 +29,8 @@ while True:
         else:
             print(f'Valor final \033[33m{saldo:.2f}\033[0m número de jogadas ganhas {ganhas} e perdidas \033[31m{percas}\033[0m')
             break
-    elif d3 != 7 and 11:
-        print(f'São {d1} e {d2} os valores dos dados jogados')
+    elif n3 != 7 and 11:
+        print(f'São {n1} e {n2} os valores dos dados jogados')
         saldo = valor - 20
         valor = saldo
         print(f'Shiii você perdeu!\033[31mSaldo atual {saldo}\033[0m')
