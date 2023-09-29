563ac550040f781c78289efb3318ff201f9d3308
Filipi Inácio Penha dos Santos <filipiinacio25251120@gmail.com>
2023-09-29 19:49:10 -0300

    correcao1_week6

--- aulapythonJ&E/correcao1_week6.py
+++ aulapythonJ&E/correcao1_week6.py
@@ -1,6 +1,6 @@
 def validar_nota(nome):
     while True:
-        nota = float(input(f'Digite a nota de  {nome}'))
+        nota = float(input(f'Digite a nota de  {nome}: '))
         if nota >= 0 and nota <= 10:
             break
         else:
@@ -12,7 +12,7 @@ soma = 0
 aprov = 0
 
 while cont < alunos:
-    nome = input('Qual o nome do aluno:')
+    nome = input('Qual o nome do aluno: ')
     while True:
         n1 = validar_nota(nome)
         n2 = validar_nota(nome)
@@ -26,7 +26,24 @@ while cont < alunos:
         p = input('Continuar proximo: (S/N)')
         if p == 's':
             break
-soma += media
-if media >= 7:
-    aprov += 1
+        soma += media
+        if media >= 7:
+            aprov += 1
+        if cont == 1:
+            maiorMed = media
+            nome_MM = nome
+            menormed = media 
+            nome_mm = nome
+        elif media > maiorMed:
+            MM = media
+            nome_MM = nome
+        elif media < menormed:
+            mm = media
+            nome_mm = nome 
+        media_turma = soma / alunos
+        reprov = alunos - aprov
+        pa = aprov / alunos * 100
+        pr = reprov / alunos * 100
 
+print(f'Resultado: \n Aprovados: {aprov} [{pa:.2f}%] \n Reprovados: {reprov} [{pr:.2f}%]')
+print(f'\nMaior média: {maiorMed:.1f} de {nome_MM}!\nMenor média: {menormed:.1f} de {nome_mm}!')
