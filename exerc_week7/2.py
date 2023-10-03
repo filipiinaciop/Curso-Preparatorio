d350651a53de2ce4e0e52f2446a16bf5e61b4481
Filipi Inácio Penha dos Santos <filipiinacio25251120@gmail.com>
2023-10-03 09:58:43 -0300

    exercs_week7

---
+++ exerc_week7/2.py
@@ -0,0 +1,48 @@
+x = 0
+maior = float('-inf')
+menor = float('inf')
+bebe = 0
+crianca = 0
+pre = 0
+adoles = 0
+jovem = 0
+adult = 0
+idoso = 0
+
+for c in range(1, 11):
+    idade = int(input('Digite a idade: '))
+
+    if idade <= 2:
+        bebe += 1
+
+    elif idade > 2 and idade <= 10:
+        crianca += 1
+
+    elif idade > 10 and idade <= 14:
+        pre += 1
+
+    elif idade > 14 and idade < 18:
+        adoles += 1 
+
+    elif idade >= 18 and idade < 21:
+        jovem += 1 
+
+    elif idade >= 21 and idade < 60:
+        adult += 1
+
+    elif idade >= 60:
+        idoso += 1
+
+    if idade > maior:
+        maior = idade
+    if idade < menor:
+        menor = idade
+
+x += idade
+media = x / 10
+
+print()
+print(f'Bebes: {bebe}\nCrianças: {crianca}\nPré-adolecentes: {pre}\nAdolecentes: {adoles}\nJovens: {jovem}\nAdultos: {adult}\nIdosos: {idoso}')
+print()
+print(media)
+print(f'A maior idade digita foi {maior}\nA menor foi {menor}')
---
+++ exerc_week7/5.py
@@ -0,0 +1,28 @@
+from time import sleep
+
+def tempo(min, segs):
+    ts = min * 60 + segs
+
+    for c in range(ts, -1, -1):
+        min_rest = c // 60
+        segs_rest = c % 60
+        if c == 10:
+            print(f'\033[91m{min_rest:02d}:{segs_rest:02d}\033[91m')
+        else:
+            print(f'{min_rest:02d}:{segs_rest:02d} ')
+        sleep(1)
+
+segs = 0
+def main():
+    min = int(input('Qual o minuto inicial? ')) 
+    if min != 0:
+        segs = int(input('Quantos segundos? '))
+
+    if min == 0:
+        segs = int(input('Quantos segundos então? '))
+    
+    tempo(min, segs)
+
+if __name__ == "__main__":
+    main()
+print('\033[0mFIM\033[0m')
