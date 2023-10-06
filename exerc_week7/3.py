a76d9c8e0878f00415cbece87a6232fe2dd0c32d
Filipi Inácio Penha dos Santos <filipiinacio25251120@gmail.com>
2023-10-06 19:25:19 -0300

    exercs_week7

--- exerc_week7/3.py
+++ exerc_week7/3.py
@@ -1,14 +1,14 @@
 egt = int(input('Escreva o número de alunos na escola Germinare TECH: '))
 egb = int(input('Escreva o número de alunos na escola Germinare Business: '))
 
-anos = 0
+if egt > egb:
+    print('ja é maior')
+else:
+    for a in range(1, 1000):
+        egt *= 1.50
+        egb *= 1.10
 
-for a in range(1, 1000):
-    egt *= 1.50
-    egb *= 1.10
-    anos += 1
+        if egt > egb:
+            break
 
-    if egt > egb:
-        break
-
-print(f'Em {anos} anos a Germinare TECH ultrapassará a Germinare Business em alunos')
+print(f'Em {a} anos a Germinare TECH ultrapassará a Germinare Business em alunos')
