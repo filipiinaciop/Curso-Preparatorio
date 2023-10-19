ba83701d10c6d81e4a7274f65cdf982b2328afab
Filipi Inácio Penha dos Santos <filipiinacio25251120@gmail.com>
2023-10-19 17:32:24 -0300

    week9

--- week9/1.py
+++ week9/1.py
@@ -1,33 +1,13 @@
-p = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-#OU
-# p = [0]*9
-#1ª posição: $200 - $299
-#2ª posição: $300 - $399
-#3ª posição: $400 - $499
-#4ª posição: $500 - $599
-#5ª posição: $600 - $699
-#6ª posição: $700 - $799
-#7ª posição: $800 - $899
-#8ª posição: $900 - $999
-#9ª posição: $1000 em diante
-# NãO FAÇA COM IFS ALINHADOS.
+conts = [0,0,0,0,0,0,0,0,0]
+salarios = [470, 320, 420, 590, 680, 750, 860, 920, 1050]
 
-def att(valor):
-    posicao = int((valor - 200) / 100)
-    
-    if posicao < 0:
-        posicao = 0
-    elif posicao > 8:
-        posicao = 8
-    
-    p[posicao] += 1
-    return posicao + 1
+for s in salarios:
+    indice = (s - 200) // 100
+    if 0 <= indice < len(conts):
+        conts[indice] += 1
 
-while True:
-    v = float(input('Insira um valor (ou digite uma valor negativo para encerrar): '))
-    if v < 0:
-        break
-    valor = (0.09 * v) + 200 #pega a porcentagem e ja soma
-    posicao = att(valor)
-for posicao, contagem in enumerate(p):
-    print(f'Posição {posicao + 1} - {contagem} entradas')
+for i, cont in enumerate(conts):
+    if i == 8:
+        print(f"{i + 1} posição: $1000 em diante - {cont} vendedores")
+    else:
+        print(f"{i + 1} posição: ${200 + i * 100} - ${299 + i * 100} - {cont} vendedores")
