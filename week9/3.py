d915eb4f60917343e162840e2924a5bfbd5441dc
Filipi Inácio Penha dos Santos <filipiinacio25251120@gmail.com>
2023-10-19 13:10:23 -0300

    week9

--- week9/3.py
+++ week9/3.py
@@ -1,36 +1,31 @@
-def filtrar_clientes(clientes, idade_min, idade_max, localizacao, valor_min_compras):
-    clientes_filtrados = set()
-
-    for cliente in clientes:
-        nome, idade, cidade, compras = cliente
-
+def filtrar_clientes():
+    clientes_filtrados = []
+    
+    while True:
+        nome = input("Digite o nome do cliente (ou digite 'parar' para encerrar): ")
+        if nome.lower() == 'parar':
+            break
+        idade = int(input("Digite a idade do cliente: "))
+        cidade = input("Digite a localização do cliente: ")
+        compras = float(input("Digite o valor de compras realizadas pelo cliente: R$ "))
+        
         # Verifica se o cliente atende aos critérios
         if idade_min <= idade <= idade_max and cidade == localizacao and compras >= valor_min_compras:
-            clientes_filtrados.add(cliente)
+            cliente = (nome, idade, cidade, compras)
+            clientes_filtrados.append(cliente)
 
     return clientes_filtrados
 
-# Solicita informações ao usuário
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
-localizacao = input("Digite sua localização (cidade ou estado): ")
-valor_compra = float(input("Digite o valor da compra: R$ "))
+idade_min = 25
+idade_max = 35
+localizacao = 'Sao paulo'.lower
+valor_min_compras = 1000
+clientes_filtrados = filtrar_clientes()
 
-# Base de dados fictícia de clientes
-base_de_dados = [
-    ("Cliente1", 30, "Sao Paulo", 1500),
-    ("Cliente2", 25, "Rio de Janeiro", 800),
-    ("Cliente3", 40, "Sao Paulo", 1200),
-    ("Cliente4", 28, "Sao Paulo", 900),
-]
+if clientes_filtrados:
+    print("\nClientes que atendem aos critérios:")
+    for cliente in clientes_filtrados:
+        print(cliente)
+else:
+    print("Nenhum cliente atende aos critérios especificados.")
 
-idade_min = idade - 5
-idade_max = idade + 5
-valor_min_compras = valor_compra
-
-clientes_filtrados = filtrar_clientes(base_de_dados, idade_min, idade_max, localizacao, valor_min_compras)
-
-# Exibir os clientes filtrados
-print("\nClientes filtrados:")
-for cliente in clientes_filtrados:
-    print(cliente)
