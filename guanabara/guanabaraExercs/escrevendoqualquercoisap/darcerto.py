9b784363c9d394d8edcce5c5c9942ada9bc9dcd7
Filipi Inácio Penha dos Santos <filipiinacio25251120@gmail.com>
2023-11-05 17:49:41 -0300

    guanabara

--- guanabara/guanabaraExercs/escrevendoqualquercoisap/darcerto.py
+++ guanabara/guanabaraExercs/escrevendoqualquercoisap/darcerto.py
@@ -1,4 +1,4 @@
 sexo = input('Informe seu sexo: [M/F] ').strip().upper()[0]
 while sexo not in 'MmFf':
     sexo = input('Dados invalidos. Por favor informe seu sexo: ').strip().upper()[0]
-print(f'Seu {sexo} registrado com sucesso.')
+print(f'Sexo {sexo} registrado com sucesso.')
